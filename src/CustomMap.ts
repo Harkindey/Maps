9b462e7afc445190ef1ca8bd24ee922f52b1fefc
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-23 12:02:14 +0100

    CustomMap

---
+++ src/CustomMap.ts
@@ -0,0 +1,13 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(public divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 5,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+}
